c9d860f66e07f1bc9a034f6e6cbd48a493f459a8
antonstrand <jag@antonstrand.se>
2019-11-20 12:54:26 +0100

    Adding word count to the json data

--- server/scripts/convertDatasetToJSON.js
+++ server/scripts/convertDatasetToJSON.js
@@ -13,19 +13,40 @@ const writeFile = file => data => node (done => fs.writeFile (file, data, done))
 /** fromNullable :: a -> Fluture a a */
 const fromNullable = ifElse(R.isNil, reject, resolve)
 
+const notEmptyRow = row => row !== ''
+
+/** toRow :: String -> [String] */
+const toRow = R.split('\n')
+
+/** toColumn :: [String] -> [[String]] */
+const toColumn = R.map (R.split ('\t'))
+
+/** toWords :: [[String]] -> [String] */
+const toWords = R.compose (R.tail, R.head)
+
+const toBlog = row => ({
+  title: R.head (row),
+  wordCount: R.map (Number) (R.tail (row))
+})
+
+const toFormat = rows => ({
+  wordCount: toWords (rows).length,
+  words: toWords (rows),
+  blogs: R.map (toBlog) (R.tail (rows))
+})
+
 /** main :: String -> () */
 const main = saveTo => R.pipe(
   fromNullable,
-  chain(readFile),
-  map(R.split('\n')),
-  map(R.map(R.split('\t'))),
-  map(R.filter(row => R.head(row) !== '')),
-  map(rows => ({ words: R.tail(R.head(rows)), blogs: R.tail(rows).map(R.zipObj(R.head(rows)))})),
-  map(data => ({ ...data, blogs: data.blogs.map(row => ({ title: row.Blog, wordCount: R.tail(Object.values(row)).map(Number)}))})),
-  map(JSON.stringify),
-  chain(writeFile(saveTo)),
-  fork(e => console.log('The conversion failed.\n', e, '\n'))
-      (() => console.log('The data has been converted to a JSON file\n'))
+  chain (readFile),
+  map (toRow),
+  map (R.filter (notEmptyRow)),
+  map (toColumn),
+  map (toFormat),
+  map (JSON.stringify),
+  chain (writeFile (saveTo)),
+  fork (e => console.log('\nThe conversion failed.\n', e, '\n'))
+       (() => console.log('\nThe data has been converted to a JSON file\n'))
 )
 
-main(process.argv[3])(process.argv[2])
+main (process.argv[3]) (process.argv[2])
