3c32812dffb51fe17a863713fe4c6954b49f161e
antonstrand <jag@antonstrand.se>
2019-11-20 12:55:59 +0100

    Implement pearson algorithm

---
+++ server/src/models/metrics/pearson.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** pearson :: Number -> Blog -> Blog -> Number */
+const pearson = numOfWords => blogA => blogB => {
+  let sumA, sumB, sumAsq, sumBsq, pSum, cntA, cntB
+  sumA = sumB = sumAsq = sumBsq = pSum = 0
+
+  // sum up the word counts for each blog
+  for(let i = 0; i < numOfWords; i++) {
+    cntA = blogA.wordCount[i]
+    cntB = blogB.wordCount[i]
+    sumA += cntA
+    sumB += cntB
+    sumAsq += cntA**2
+    sumBsq += cntB**2
+    pSum += cntA * cntB
+  }
+
+  const num = pSum - (sumA * sumB / numOfWords)
+  const den = sqrt((sumAsq - sumA**2 / numOfWords) * (sumBsq - sumB**2 / numOfWords))
+
+  // Invert the pearson score
+  return 1 - num/den
+}
