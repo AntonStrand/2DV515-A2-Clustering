8b7c3eda738b198e5fee47606c8f0f5bfda421df
Anton Strand <jag@antonstrand.se>
2019-11-24 16:07:40 +0100

    Refactoring

--- server/src/models/centroid.js
+++ server/src/models/centroid.js
@@ -1,26 +1,23 @@
-// const Centroid = ({
-//   of: {
-//     wordCount: Object,
-//     assignments: []
-//   }
-// })
-
-const of = (wordCount) => (assignments) => ({ wordCount, assignments })
-const empty = () => ({ wordCount: {}, assignments: [] })
+/**
+ * Centroid :: { wordCount :: {"Number", Number}, assignments :: [Blog] }
+ */
 
 const { lensPath, lensProp, set } = require ('ramda')
 
 const assignments = lensProp ('assignments')
 
+/** empty :: () -> Centroid */
+const empty = () => ({ wordCount: {}, assignments: [] })
+
 /** setWordCount :: Number -> Number -> Centriod -> Centriod */
 const setWordCount = wordId => position => 
   set (lensPath ([ 'wordCount', `${wordId}` ])) (position)
 
+/** clearAssignments :: Centroid -> Centroid */
 const clearAssignments = set (assignments) ([]) 
 
 module.exports = {
   setWordCount,
   clearAssignments,
-  of,
   empty
 }
