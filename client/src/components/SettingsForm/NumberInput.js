12c15297fbf7f7fa7be7aa61d4845ee49b3e44d1
Anton Strand <jag@antonstrand.se>
2019-11-25 18:13:43 +0100

    refactor to match eslint rules

--- client/src/components/SettingsForm/NumberInput.js
+++ client/src/components/SettingsForm/NumberInput.js
@@ -1,32 +1,32 @@
 import Type from 'union-type'
 
-const isUint = x => Number.isInteger(x) && x > 0
+const isUint = x => Number.isInteger (x) && x > 0
 const isEmpty = x => x.length === 0
 
-const NumberInput = Type({
+const NumberInput = Type ({
   Valid: [Number],
   Invalid: [],
   Default: []
 })
 
 const toNumberInput = n =>
-  !isEmpty(n) && isUint(Number(n))
-    ? NumberInput.Valid(Number(n))
-    : isEmpty(n)
+  !isEmpty (n) && isUint (Number (n))
+    ? NumberInput.Valid (Number (n))
+    : isEmpty (n)
       ? NumberInput.Default
       : NumberInput.Invalid
 
 /** isValidNumberInput :: InputNumber -> Boolean */
-const isValidNumberInput = NumberInput.case({
+const isValidNumberInput = NumberInput.case ({
   Valid: () => true,
   _: () => false
 })
 
 /** getNumber :: InputNumber -> Number */
-const getNumber = NumberInput.case({ Valid: n => n })
+const getNumber = NumberInput.case ({ Valid: n => n })
 
 /** showError :: InputNumber -> String | Boolean */
-const showError = NumberInput.case({
+const showError = NumberInput.case ({
   Invalid: () => 'It has to be a positive integer.',
   _: () => false
 })
