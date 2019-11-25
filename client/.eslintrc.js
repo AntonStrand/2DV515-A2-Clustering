850eb4da5f5f092dec82f894ce8e3b64106dff2f
Anton Strand <jag@antonstrand.se>
2019-11-25 19:06:58 +0100

    Don't require displayName

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-    "func-call-spacing": ["error", "always", { allowNewlines: true }]
+    "func-call-spacing": ["error", "always", { allowNewlines: true }],
+    "react/display-name": ["off"]
   },
   settings: {
       react: {
