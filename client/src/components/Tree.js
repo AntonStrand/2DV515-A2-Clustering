66c383955056e0347f213388967ef5c7e011fcbb
Anton Strand <jag@antonstrand.se>
2019-11-26 21:53:31 +0100

    It's now possible to expand/collapse branches

--- client/src/components/Tree.js
+++ client/src/components/Tree.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const toJSX = cluster =>
-  cluster.children
-    ? <ul>{cluster.children.map (toJSX)}</ul>
-    : <li>{cluster.title}</li>
+const Structure = ({ cluster }) => {
+  const [isActive, setActive] = useState (false)
+  const toggleActive = () => setActive (!isActive)
 
-const Tree = ({ cluster }) => {
-  return <div>{toJSX (cluster)}</div>
+  return cluster.children
+    ? <ul>
+      <li><span onClick={toggleActive} className={isActive ? 'caret caret-down' : 'caret'}>Cluster</span>
+        <ul className={isActive ? 'nested active caret-down' : 'nested'}>
+          {cluster.children.map ((c, i) => <Structure key={i} cluster={c} />)}
+        </ul>
+      </li>
+    </ul>
+    : <li className={'blog-title'}>{cluster.title}</li>
 }
+
+Structure.propTypes = {
+  cluster: PropTypes.object
+}
+
+const Tree = ({ cluster }) => <div><Structure cluster={cluster}/></div>
 
 Tree.propTypes = {
   cluster: PropTypes.object
